8408a62e78c790e44d4868f3a74feebddb7b6f4a
Jordy Koppen <24296556+jordykoppen@users.noreply.github.com>
2025-06-01 20:27:11 +0200

    refactor: update cornelius keyboard

--- keyboards/qmk/jornelius/keymaps/default/keymap.c
+++ keyboards/qmk/jornelius/keymaps/default/keymap.c
@@ -16,18 +16,33 @@
 #include QMK_KEYBOARD_H
 
 enum layers {
-   _DEFAULT,
-   _LOWER,
-   _RAISE,
-   _ADJUST
-}
+    _DEFAULT = 0,
+    _LOWER = 1,
+    _RAISE = 2,
+    _ADJUST = 3,
+};
 
 #define LOWER MO(_LOWER)
 #define RAISE MO(_RAISE)
+#define ADJST MO(_ADJUST)
+
+// Mod Tap defintion
+#define MT_A LGUI_T(KC_A)
+#define MT_S LCTL_T(KC_S)
+
+#define MT_SCLN RGUI_T(KC_SCLN)
+#define MT_QUOT RALT_T(KC_QUOT)
+
 #define CTLESC LCTL_T(KC_ESC)
 
-#define ADJTRNS LALT(LGUI(KC_TRNS))
+#define AGLEFT LALT(LGUI(KC_LEFT))
+#define AGRIGHT LALT(LGUI(KC_RIGHT))
+#define AGUP LALT(LGUI(KC_UP))
+#define AGDOWN LALT(LGUI(KC_DOWN))
+#define AGF LALT(LGUI(KC_F))
+#define AGC LALT(LGUI(KC_C))
 
+#define FLOW_TAP_TERM 200
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    /* Qwerty
@@ -42,30 +57,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     * `------------------------------------------------------------------------------------------'
     */
   [_DEFAULT] = LAYOUT(
-      KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,         KC_Y,    KC_U,     KC_I,    KC_O,   KC_P,  KC_BSPC,
-     CTLESC,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,         KC_H,    KC_J,     KC_K,    KC_L, KC_SCLN, KC_QUOT,
-     KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,         KC_N,    KC_M,  KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,
-      KC_ESC, KC_LCTL, KC_LGUI, KC_LALT, TL_LOWR,  KC_SPC,       KC_ENT, TL_UPPR,  KC_RALT, KC_RGUI, KC_RCTL,  KC_ESC
-  ),                                                           
-                                                               
-  [_LOWER] = LAYOUT(                                           
+      KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,         KC_Y,    KC_U,     KC_I,    KC_O,    KC_P, KC_BSPC,
+      CTLESC,    MT_A,    MT_S,    KC_D,    KC_F,    KC_G,         KC_H,    KC_J,     KC_K,    KC_L, MT_SCLN, MT_QUOT,
+      KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,         KC_N,    KC_M,  KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,
+      KC_ESC,  KC_LCTL,KC_LALT, XXXXXXX,   LOWER,  KC_SPC,       KC_ENT,   RAISE,  KC_RGUI, KC_RALT, KC_RCTL,  KC_ESC
+  ),
+  [_LOWER] = LAYOUT(
      _______,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,         KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_BSPC,
      _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, XXXXXXX, XXXXXXX,
      _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-     _______, _______, _______, _______, _______, _______,      _______, _______, _______, _______, _______, _______
-  ),                                                           
-                                                               
-  [_RAISE] = LAYOUT(                                           
+     _______, _______, _______, _______,   LOWER, _______,      _______,   ADJST, _______, _______, _______, _______
+  ),
+  [_RAISE] = LAYOUT(
      _______, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
      _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      KC_MINS,  KC_EQL, KC_LCBR, KC_RCBR, KC_PIPE,  KC_GRV,
      _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      KC_UNDS, KC_PLUS, KC_LBRC, KC_RBRC, KC_BSLS, KC_TILD,
-     _______, _______, _______, _______, _______, _______,      _______, _______, _______, _______, _______, _______
-  ),                                                           
-                                                               
-  [_ADJUST] = LAYOUT(                                          
+     _______, _______, _______, _______,   ADJST, _______,      _______,   RAISE, _______, _______, _______, _______
+  ),
+  [_ADJUST] = LAYOUT(
      _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-     QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      ADJTRNS, ADJTRNS, ADJTRNS, ADJTRNS, XXXXXXX, XXXXXXX,
-     _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-     _______, _______, _______, _______, _______, _______,      _______, _______, _______, _______, _______, _______
+     QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, AGF, XXXXXXX,      AGLEFT, AGDOWN, AGUP, AGRIGHT, XXXXXXX, XXXXXXX,
+     _______, XXXXXXX, XXXXXXX, AGC, XXXXXXX, XXXXXXX,      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+     _______, _______, _______, _______,   ADJST, _______,      _______,   ADJST, _______, _______, _______, _______
   ),
 };
+
