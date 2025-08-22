69ddfed7036e8550aa1f6d0907e0fc86df3dd4af
Jordy Koppen <24296556+jordykoppen@users.noreply.github.com>
2025-08-22 14:12:35 +0200

    fix(deltalab): update SSH user from root to jordy

--- fish/functions/deltalab.fish
+++ fish/functions/deltalab.fish
@@ -1,5 +1,5 @@
 function deltalab
     kitty @ set-tab-color active_bg=#FF5F00 inactive_bg=#FF966C inactive_fg=#080808
-    ssh root@deltalab -t fish
+    ssh jordy@deltalab
     kitty @ set-tab-color active_bg=#C6A0F6 inactive_bg=#1E2030 inactive_fg=#CAD3F5
 end
