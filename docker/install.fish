03d37ad257d871016e83414f342e99330797a4d5
Jordy Koppen <24296556+jordykoppen@users.noreply.github.com>
2025-09-08 12:16:50 +0200

    feat(docker): update Brewfile and config for OrbStack integration; remove colima references

--- docker/install.fish
+++ docker/install.fish
@@ -2,9 +2,6 @@ if not type -q docker
     echo "→ docker not found. Installing now..."
     brew bundle --file=$DOTFILES/docker/Brewfile
 
-    echo "→ Enabling docker/colima at startup"
-    brew services start colima
-
     echo "→ Symlinking docker config"
     ln -sfn $DOTFILES/docker/config.json $HOME/.docker/config.json
 else
