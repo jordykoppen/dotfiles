c090a56257a6405ccc6ed1ce43d53a17535e44bf
Jordy Koppen <24296556+jordykoppen@users.noreply.github.com>
2025-12-20 17:02:40 +0100

    style(keymap): update NOMODS layer to  use transparent mappings

--- keyboards/qmk/jornelius/keymaps/jordy/keymap.c
+++ keyboards/qmk/jornelius/keymaps/jordy/keymap.c
@@ -134,9 +134,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     * `------------------------------------------------------------------------------------------'
     */
   [_NOMODS] = LAYOUT(
-      KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,         KC_Y,    KC_U,     KC_I,    KC_O,    KC_P, KC_BSPC,
-      CTLESC,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,         KC_H,    KC_J,     KC_K,    KC_L, KC_SCLN, KC_QUOT,
-      KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,         KC_N,    KC_M,  KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,
-      KC_HYPR,KC_LCTL, KC_LALT, XXXXXXX,   LOWER,  KC_SPC,       KC_ENT,   RAISE,  XXXXXXX, KC_RALT, KC_RCTL, KC_HYPR
+      _______, _______, _______, _______, _______, _______,      _______, _______, _______, _______, _______, _______,
+      _______,   KC_A,    KC_S,    KC_D, _______, _______,      _______, _______,   KC_K,    KC_L, KC_SCLN, _______,
+      _______, _______, _______, _______, _______, _______,      _______, _______, _______, _______, _______, _______,
+      _______, _______, _______, _______, _______, _______,      _______, _______, _______, _______, _______, _______
   ),
 };
